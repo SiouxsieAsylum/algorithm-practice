c6ab0022969924b0a0c98efaa5dbd7311f530abe
Andrea McKenzie <mckenzie.andrea.m@gmail.com>
2019-05-31 13:28:19 -0400

    closure version

--- twoSum.js
+++ twoSum.js
@@ -1,15 +1,34 @@
-// Given an array of integers, 
-//return indices of the two numbers such that they add up to a specific target.
+// Closure Version
+function twoSum(){
+    let startingIndex = 0;
+    let resultArray = []
+     function innerFunction(array, integer){
+        let startNum = array.shift();
+        let subtractedNum = integer - startNum;
+        let found = array.findIndex(function (element){
+            return element === subtractedNum;
+        })
+    
+        if (found >= 0) {
+            //found is the index of the shortened array
+            //add 1 for the initial shift
+            //add starting index, which doubles to track the amount of shifts thus far
+            let endingIndex = found + 1 + startingIndex;
+            return [startingIndex, endingIndex];
+        } else {
+            startingIndex++;
+            return innerFunction(array, integer);
+        }
+    }
+    return innerFunction;
+}
 
-// given a number
-// shift first number
-// subtract a the first number and save that index 
-// loop through and see what number is equal to the remainder
-// if none, increase startingIndex and go through the loop again
+const result = twoSum()
+console.log(result([4,2,5,7,8,1,9,12,3], 15))
 
+// Unwrapped Version
 let startingIndex = 0;
 function twoSum(numArr, target){
-    //figure out how to initialize this and carry the index
     let startNum = numArr.shift();
     let subtractedNum = target - startNum;
     let found = numArr.findIndex(function (element){
@@ -23,9 +42,9 @@ function twoSum(numArr, target){
         let endingIndex = found + 1 + startingIndex
         return [startingIndex, endingIndex];
     } else {
-            startingIndex++;
-            twoSum(numArr, target);
+        startingIndex++;
+        return twoSum(numArr, target);
     }
 }
 
-twoSum([4,2,5,7,8,1,9,12,3], 15)
+ console.log(twoSum([4,2,5,7,8,1,9,12,3], 15))
